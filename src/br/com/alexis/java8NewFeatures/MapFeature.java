1e3f8f4244fd046f5b2b755cbec48308d6349dae
alexisbrabo <alexis_brabo@hotmail.com>
2019-03-07 17:12:11 -0300

    Improvment in map feature

--- src/br/com/alexis/java8NewFeatures/MapFeature.java
+++ src/br/com/alexis/java8NewFeatures/MapFeature.java
@@ -6,18 +6,26 @@ import java.util.stream.Stream;
 public class MapFeature {
 
 	public static void main(String[] args) {
-		//Initialize array
-		String[] stringArray = new String[] {"Alexis", "Brena", "Bilbo"};
-		//Convert Array to String
+		// Initialize first array
+		String[] stringArray = new String[] { "Alexis", "Brena", "Bilbo" };
+		// Convert Array to String
 		Stream<String> stringStream = Arrays.stream(stringArray);
-		
-		//Create a new Stream transforming all Strings in stringStream to UpperCase
+
+		// Create a new Stream transforming all Strings in stringStream to UpperCase
 		Stream<String> uppercaseStringStream = stringStream.map(s -> s.toUpperCase());
-		
-		//Convert the Stream to Array
+
+		// Convert the Stream to Array
 		String[] uppercaseStringArray = uppercaseStringStream.toArray(String[]::new);
-		
-		//Show the array with all Strings in UpperCase
+
+		// Show the array with all Strings in UpperCase
 		System.out.println(Arrays.toString(uppercaseStringArray));
+
+		// Initialize second array
+		String[] stringArray2 = new String[] { "Alexis", "Brena", "Bilbo", "Adam", "Pantera" };
+		
+		//Same thing that above but with chained methods
+		System.out.println(Arrays.toString(Arrays.stream(stringArray2)
+				.map(s -> s.toUpperCase())
+				.toArray()));
 	}
 }
